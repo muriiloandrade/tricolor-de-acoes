44785c2a26e0675773cd0ee6938109d7c76d5fc2
Murilo <muriloandradealmeida@gmail.com>
2020-11-15 19:22:03 -0300

    Homepage ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RootComponent } from './pages/root/root.component';
 import { HeaderComponent } from './components/header/header.component';
+import { EquipeComponent } from './components/equipe/equipe.component';
+import { ProjetoComponent } from './components/projeto/projeto.component';
 import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
-  declarations: [AppComponent, RootComponent, HeaderComponent, BannerComponent],
+  declarations: [
+    AppComponent,
+    RootComponent,
+    HeaderComponent,
+    BannerComponent,
+    ProjetoComponent,
+    EquipeComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
