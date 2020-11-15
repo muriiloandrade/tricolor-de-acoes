2b867fa30673b1be20856119990a60863923c272
Murilo <muriloandradealmeida@gmail.com>
2020-11-15 17:15:02 -0300

    Banner component Ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RootComponent } from './pages/root/root.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
-  declarations: [AppComponent, RootComponent, HeaderComponent],
+  declarations: [AppComponent, RootComponent, HeaderComponent, BannerComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
